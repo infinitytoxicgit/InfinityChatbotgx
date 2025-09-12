f6b56b6501e82d7f310ceb5fc5f01bf6b09955ec
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-09-12 23:41:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,10 @@ API_ID = "27209067"
 API_HASH = "0bb2571bd490320a5c9209d4bf07902e"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-STRING1 = getenv("BQFYUd4AaBQ-7rxbKMguDsyH8oJPUtsDN4JBK4J-P09mTvQkHm24IsRuD3VtdQi1nbxvR97mcLHog1Ke9s6dKIYskILAyeaNzfr7CayVeDm_hiY0XKz9Ge7BR8A4KXBFdpUy_9KA7t0yDheKWcdJOM-ArBjs40uUUIjyPd0tntz3tWArfLmttWkuUNI2LGmTn_EjZjBH_XRopuZNVsesynDa7C5z8a-5V_Z87OKlzkoQL8u5Tw34EYMA_0eBsdjuEs5-rU2gz80eDSdIf9e0Pf7vWxy8tPxRjNe6DROsj5-m-CEys1SIoF_Uaw0jCZo8lZRRFk_ezLpnp2zclT0dk3iCxJDPeQAAAAHjZt-vAA", None)
-MONGO_URL = getenv("mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+STRING1 = getenv("STRING1", "")
+MONGO_URL =
+getenv("mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
 OWNER_ID = int(getenv("OWNER_ID", "7487670897"))
 SUPPORT_GRP = "https://t.me/+zyocLmf3JvIwOWY9"
-UPDATE_CHNL = "@dark_x_knight_musiczz_support"
+UPDATE_CHNL = "dark_x_knight_musiczz_support"
 OWNER_USERNAME = "DarkGamer7t2rI"
